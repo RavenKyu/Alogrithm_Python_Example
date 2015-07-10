ae02bde50761143d56af8c4e58ef5e601ae218f8
Raven <hong18s@gmail.com>
2015-07-10 11:17:52 +0900

    동시 키입력 해결 * 이벤트 필러를 사용하여 키를 동시에 입력 받을 수 있게 하였다.

--- 2_crash_algorithm/bounce_circle/bounce_circle.py
+++ 2_crash_algorithm/bounce_circle/bounce_circle.py
@@ -27,6 +27,9 @@ class Ball():
         # 상태
         self.is_touched = False
 
+        self.img = QtGui.QImage('face.png')
+        self.img = self.img.scaled(self.r, self.r)
+
     def move(self):
         """볼의 이동 값을 구한다."""
         # 볼이 움직일 수 있는 값을 구한다
@@ -56,11 +59,53 @@ class Ball():
         if self.is_touched:
             pen = QtGui.QPen(QtCore.Qt.red, 2, QtCore.Qt.SolidLine)
         else:
-            pen = QtGui.QPen(QtCore.Qt.black, 2, QtCore.Qt.SolidLine)
+            pen = QtGui.QPen(QtCore.Qt.black, 3, QtCore.Qt.SolidLine)
 
         qp.setPen(pen)
+        qp.drawImage(self.x, self.y, self.img)
         qp.drawEllipse(self.x, self.y, self.r, self.r)
-        qp.drawPoint((self.x + self.r /2), (self.y + self.r /2))
+        # qp.drawPoint((self.x + self.r /2), (self.y + self.r /2))
+
+class Player():
+    _bg_w = 0
+    _bg_h = 0
+
+    def __init__(self):
+        # 좌표
+        self.r = 50
+        self.x = self._bg_w / 2
+        self.y = self._bg_h - self.r
+
+        self.is_touched = False
+        self.img = QtGui.QImage('face.png')
+        self.img = self.img.scaled(self.r, self.r)
+
+    def move_left(self, acc):
+        self.x -= (15 + (40 if acc is True else 0))
+
+        if self.x <= 0:
+            self.x = 0
+
+
+    def move_right(self, acc):
+        end_of_ww = (self._bg_w - self.r)
+        self.x += (15 + (40 if acc is True else 0))
+
+        if self.x >= end_of_ww:
+            self.x = end_of_ww
+
+
+    def draw(self, qp):
+        # 원을 그리는 함수
+        if self.is_touched:
+            pen = QtGui.QPen(QtCore.Qt.red, 2, QtCore.Qt.SolidLine)
+        else:
+            pen = QtGui.QPen(QtCore.Qt.black, 1, QtCore.Qt.SolidLine)
+
+        qp.setPen(pen)
+        qp.drawImage(self.x, self.y, self.img)
+        qp.drawEllipse(self.x, self.y, self.r, self.r)
+        # qp.drawPoint((self.x + self.r /2), (self.y + self.r /2))
 
 
 class Form(QtWidgets.QWidget):
@@ -70,16 +115,22 @@ class Form(QtWidgets.QWidget):
         self.mouse_x = 0
         self.mouse_y = 0
         self.setMouseTracking(True)
+        self.installEventFilter(self)
 
         self.init_objects()
-
+        self.score = 0
         self.rt = QtCore.QBasicTimer()
         self.rt.start(10, self)
-
+        self.pressed_key = []
 
     def init_objects(self):
         self.ww = self.width()
         self.wh = self.height()
+
+        Player._bg_w = self.ww
+        Player._bg_h = self.wh
+        self.player = Player()
+
         Ball._bg_w = self.ww
         Ball._bg_h = self.wh
 
@@ -95,12 +146,36 @@ class Form(QtWidgets.QWidget):
                 Ball(x, y, r)
             )
 
+
+    def eventFilter(self, QObject, QEvent):
+        self.pressed_key = list(set(self.pressed_key))
+        if QEvent.type() == QEvent.KeyPress:
+            self.pressed_key.append(QEvent.key())
+
+            if self.pressed_key.count(QtCore.Qt.Key_Left) and \
+                self.pressed_key.count(QtCore.Qt.Key_Space):
+                self.player.move_left(True)
+
+            elif self.pressed_key.count(QtCore.Qt.Key_Right) and \
+                self.pressed_key.count(QtCore.Qt.Key_Space):
+                self.player.move_right(True)
+
+        elif QEvent.type() == QEvent.KeyRelease:
+            self.pressed_key.remove(QEvent.key())
+
+        return False
+
     def keyPressEvent(self, QKeyEvent):
         if QKeyEvent.key() == QtCore.Qt.Key_Escape:
             self.init_objects()
 
+        if QKeyEvent.key() == QtCore.Qt.Key_Left:
+            self.player.move_left(False)
+        elif QKeyEvent.key() == QtCore.Qt.Key_Right:
+            self.player.move_right(False)
+
     def timerEvent(self, e):
-        self.ball_moving()
+        self.moving()
         self.repaint()
 
 
@@ -109,9 +184,13 @@ class Form(QtWidgets.QWidget):
         self.mouse_y = QMouseEvent.y()
 
 
-    def ball_moving(self):
+    def moving(self):
+        self.score += 0.1
+
+        # Ball 이동 계산
         for b in self.e:
             b.move()
+
 
 
     def paintEvent(self, QPaintEvent):
@@ -119,6 +198,7 @@ class Form(QtWidgets.QWidget):
         qp.begin(self)
         self.drawInfo(QPaintEvent, qp)
         self.draw_balls(QPaintEvent, qp)
+        self.draw_player(QPaintEvent, qp)
         qp.end()
 
 
@@ -126,11 +206,13 @@ class Form(QtWidgets.QWidget):
         for e in self.e:
             e.draw(qp)
 
+    def draw_player(self, event, qp):
+        self.player.draw(qp)
 
     def drawInfo(self, event, qp):
         qp.setPen(QtGui.QColor('black'))
-        qp.setFont(QtGui.QFont('Decorative', 11))
-        qp.drawText(10, 10, "mX: %d mY: %d" % (self.mouse_x, self.mouse_y))
+        qp.setFont(QtGui.QFont('Decorative', 14))
+        qp.drawText(10, 15, "Score - %d" % self.score)
 
 
 
